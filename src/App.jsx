073870cb02bd01f0c23c120a1547091ea2de98c2
EdwinRodger <codevaidik@gmail.com>
2024-09-09 13:35:14 +0530

    added adsense in head and rebuild project

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Footer from "./Footer/Footer"
 function App() {
     return (
         <>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5022218365217087"
-                crossorigin="anonymous"></script>
             <Header />
             <Main />
             <Footer />
