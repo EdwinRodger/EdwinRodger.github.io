9112409edf32d1cfb5a308143a3e632a2dfecfd6
EdwinRodger <codevaidik@gmail.com>
2024-09-09 13:29:33 +0530

    added adsense script

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Header from "./Header/Header"
 import Main from "./Main/Main"
 import Footer from "./Footer/Footer"
 function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5022218365217087"
+                crossorigin="anonymous"></script>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
 }
 
 export default App
