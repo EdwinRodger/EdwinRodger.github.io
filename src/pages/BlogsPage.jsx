d18adf96307b58c3acc5eece35a5d9f72bd06920
EdwinRodger <codevaidik@gmail.com>
2024-09-16 13:20:02 +0530

    coming soon...

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -7,6 +7,7 @@ const BlogsPage = () => {
             <Navbar />
             <div className="container py-5">
                 <h1>BlogsPage</h1>
+                <p>Coming soon...</p>
             </div>
             <Footer />
         </div >
